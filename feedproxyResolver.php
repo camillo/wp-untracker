de5b46463d44049508159c2b80914d3d04ed7b2e
Daniel Marohn <daniel.marohn@googlemail.com>
2012-06-23 08:28:05 +0200

    added options for replacement mode

--- feedproxyResolver.php
+++ feedproxyResolver.php
@@ -8,6 +8,7 @@ Author URI: https://github.com/camillo/
 Version: 0.1
 License: public domain
 } */
+//require_once ('./options.php');
 
 // take care for logging helper function _log
 if(!function_exists('_log'))
@@ -32,12 +33,15 @@ if(!function_exists('_log'))
 	}
 }
 
+// register settings related staff
+include_once dirname( __FILE__ ) . '/options.php';
+
 /**
  * Do a GET request against given url and returns the redirect_url header, if exists.
  * @return redirect_url if exists, $url otherwise; leave $url unmodified if something went wrong.
  * @param string $url
  */
-function freeUrl($url)
+function resolveUrl($url)
 {
 	try 
 	{
@@ -53,6 +57,163 @@ function freeUrl($url)
 		_log("error freeing url $url: " . $ex->getMessage());
 		return $url;
 	}
+}
+
+/**
+ * Removes a single known tracking parameters from given GET parameters.
+ * @param string $parameter the parameter's part of an url.
+ * @param string $wellKnownParameter a single parameter name to remove from $parameter
+ * @return given $parameter without $wellKnownParameter
+ */
+function removeWellKnownParameter($parameter, $wellKnownParameter)
+{
+	try
+	{
+		$pattern ="~&?" . $wellKnownParameter ."=[^&/]+~";
+		$ret = preg_replace($pattern, "", $parameter);
+		return $ret;
+	} catch (Exception $ex)
+	{
+		_log("error, removing single parameter [$parameter] :" . $ex->getMessage());
+		return $parameter;
+	}	
+}
+
+/**
+ * Removes all known tracking parameters from given GET parameters.
+ * @param string $parameter the parameter's part of an url.
+ * @param string $wellKnownParameter space separated list of parameters to remove from $parameter
+ * @return string given parameters without known tracking ones.
+ */
+function removeWellKnownParameters($parameter, $wellKnownParameter)
+{
+	$ret = $parameter;
+	try 
+	{
+		foreach (explode(" ", $wellKnownParameter) as $currentParameter)
+		{
+			$ret = removeWellKnownParameter($ret, $currentParameter);
+		}
+		_log("remove known parameters [$parameter] -> [$ret]");
+		return $ret;
+	} catch (Exception $ex)
+	{
+		_log("error removing well known parameter: " . $ex -> getMessage());
+		return $parameter;
+	}
+}
+
+/**
+ * Removes single ? in case that all parameter got removed.
+ * @param string $parameter parameter part from an url
+ * @return $parameter if there are any; empty string otherwise
+ */
+function cleanupParameters($parameter)
+{
+	try
+	{
+		$ret = $parameter;
+		if ($parameter == "?")
+		{
+			$ret = "";
+		}elseif ($parameter == "?/")
+		{
+			$ret = "/";
+		}
+		return $ret;
+	} catch (Exception $ex)
+	{
+		_log("error in cleanupParameter for parameter [$parameter]: " . $ex->getMessage());
+		return $parameter;
+	}
+}
+
+/**
+ * Cut knwon parameters from target url.
+ * @param string $url target url
+ * @return url, without any known feedproxy parameters
+ */
+function doSoftcore($url)
+{
+	try 
+	{
+		$ret = $url;
+		$parameterStart = strpos($ret, "?");
+		if ($parameterStart === false)
+		{
+			_log("no GET parameter; nothing to do.");
+		} else
+		{
+			$parameter = substr($ret, $parameterStart);
+			$wellKnownParameter = get_option('wellKnownParameter');
+			$newParameter = removeWellKnownParameters($parameter, $wellKnownParameter);
+			$newParameter = cleanupParameters($newParameter);
+			$ret = substr($ret, 0, $parameterStart) . $newParameter;
+		}
+		_log("do softcore: [$url] -> [$ret]");
+		return $ret;
+	} catch (Exception $ex)
+	{
+		_log("error in softcore: " . $ex->getMessage());
+		return $url;
+	}
+}
+
+/**
+ * Remove all GET parameters from url.
+ * @param string $url target url
+ * @return Url, without any GET parameters.
+ */
+function doHardcore($url)
+{
+	try 
+	{
+		$ret = $url;
+		$parts = split("[?]", $ret);
+		$ret = $parts[0];
+		_log("do hardcore: [$url] -> [$ret]");
+		return $ret;
+	} catch (Exception $ex)
+	{
+		_log("error in hardcore: " . $ex->getMessage());
+		return $url;
+	}
+}
+
+/**
+ * Remove google feedproxy link from given $url.
+ * 1. get new url
+ * 2. short url in case of hardcore or softcore modus.
+ * 
+ * softcore:
+ * cut knwon parameters from target url
+ * 
+ * hardcore:
+ * remove all GET parameters from url
+ * 
+ * @param string $url the url to free
+ * @return 'real' url		
+
+ */
+function freeUrl($url)
+{
+	$ret = resolveUrl($url);
+	try
+	{
+		$replacementMode = get_option('replacementMode');
+		if ($replacementMode == "softcore")
+		{
+			$ret = doSoftcore($ret);
+		} else if ($replacementMode == "hardcore")
+		{
+			$ret = doHardcore($ret);
+		}
+	} catch (Exception $ex)
+	{
+		_log("error in cutter: " . $ex->getMessage());
+	}
+	
+	return $ret;
 }
 
 /**
@@ -87,3 +248,5 @@ function untrackPost($content)
 
 // register plugin
 add_filter('content_save_pre','untrackPost');
+
+?>
---
+++ options.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * check that option ReplaceMode is valid
+ * @param string $input
+ * @return $input, if ok; default value otherwise;
+ */
+function validateReplaceMode($input)
+{
+	$ret = "full";
+	if (($input == "hardcore") || ($input == "softcore"))
+	{
+		$ret = $input;
+	}
+	_log("validate [$input] -> [$ret]");
+	return $ret;
+}
+
+/**
+ * Set parameter to default value, if no one is set.
+ * @param string $input new value
+ * @return default parameter if $input is empty; $input otherwise
+ */
+function validateWellKnownParameter($input)
+{
+	$ret = $input;
+	if (empty($input))
+	{
+		$ret = "utm_medium utm_source utm_campaign";
+	}
+	return $ret;
+}
+
+/**
+ * Register the needed settings for feedproxy resolver. 
+ */
+function registerFeedproxySettings()
+{
+	register_setting( 'feedproxyResolver', 'replacementMode', 'validateReplaceMode');
+	register_setting( 'feedproxyResolver', 'wellKnownParameter', 'validateWellKnownParameter');
+}
+
+/**
+ * Do all settings related stuff to wordpress.
+ * 1. Register the menu entry.
+ * 2. Hook into admin init to register our settings.
+ */
+function initFeedproxySettings()
+{
+	add_menu_page('FeedproxyResolver Settings', 'FeedproxyResolver', 'administrator', __FILE__, 'feedproxyOptionDrawPage');
+	add_action('admin_init', 'registerFeedproxySettings');
+}
+
+/**
+ * Draw the menu page itself
+ */
+function feedproxyOptionDrawPage() {
+?>
+    <div class="wrap">
+        <h2>FeedProxy Options</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields('feedproxyResolver'); ?>
+            <?php $replacementMode = get_option('replacementMode'); ?>
+			<?php $wellKnownParameter = get_option('wellKnownParameter'); ?>
+            <p>Choose the parameter replacement mode</p>
+			<p>
+				<input type="radio" name="replacementMode" value="full" <?php checked(  $replacementMode == "full" ); ?> > no replacement<br>
+				<input type="radio" name="replacementMode" value="softcore" <?php checked(  $replacementMode == "softcore" ); ?>> softcore<br>
+				<input type="radio" name="replacementMode" value="hardcore" <?php checked(  $replacementMode == "hardcore" ); ?>> hardcore
+			</p>
+			<p>Space separated parameter list for softcore (leave empty for default)</p>
+			<p>
+				<input type="text" name ="wellKnownParameter" style="width:500px" value="<?php echo $wellKnownParameter;?>" />
+			</p>
+            <p class="submit">
+            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+            </p>
+        </form>
+		<hr/>
+		<h3>Help</h3>
+			After the resolving of a feedproxy url, the url may contains ugly feedproxy GET parameters. You can specify, how to handle these.
+		<h4>No replacement</h4>
+			This is the savest option. The url will be used, exacly as received, containing all GET parameters.
+		<h4>softcore</h4>
+			Some known GET parameters will be removed from url, rest of them are not touched. Leave parameter textboxt empty to use default settings (utm_medium, utm_source and utm_campaign). 
+		<h4>hardcore (naming creds goes to Max)</h4>
+			All GET parameters are removed from url.  
+	</div>
+    <?php
+}
+
+add_action('admin_menu', 'initFeedproxySettings');
+
+?>
